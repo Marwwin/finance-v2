a615376b69a652a6ce8a89d8087190eaa8241b52
Marwwin <markus.kankkonen@protonmail.com>
2024-10-01 17:34:44 +0300

    added backend implementation with sqlite. Tried to fix .clj-kondo git issue

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
 (defproject my-app "0.1.0-SNAPSHOT"
   :description "Full-stack Clojure/ClojureScript app with Re-frame and Shadow-cljs"
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [ring "1.9.3"]
-                 [compojure "1.6.2"]
-                 [ring/ring-defaults "0.3.3"]
-                 [org.xerial/sqlite-jdbc "3.42.0.0"]       ;; SQLite JDBC driver
-                 [com.github.seancorfield/next.jdbc "1.3.939"]]       ;; JDBC for SQL operations
+  :dependencies [[org.clojure/clojure     "1.11.1"]
+                 [metosin/reitit          "0.7.0"]
+                 [com.github.seancorfield/next.jdbc "1.3.939"]
+                 [org.xerial/sqlite-jdbc "3.43.0.0"]
+                 [org.clojure/data.json   "2.4.0"]
+                 [ring/ring-core          "1.10.0"]
+                 [ring/ring-jetty-adapter "1.10.0"]
+                 [ring-cors               "0.1.13"]
+                 [ring/ring-json          "0.5.1"]]
   :source-paths ["src"]
   :main server.server
   :profiles {:dev {:dependencies [[cider/cider-nrepl "0.26.0"]]
--- src/server/server.clj
+++ src/server/server.clj
@@ -1,28 +1,51 @@
 (ns server.server
-  (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer [defroutes GET POST]]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [next.jdbc :as jdbc]))
+  (:gen-class)
+  (:require [reitit.ring :as ring]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.cors :refer [wrap-cors]]
+            [ring.middleware.json :as middleware]
+            [next.jdbc :as jdbc]
+            [clojure.data.json :as json]))
 
-(def db {:dbtype "h2" :dbname "finance"})
+(def ds (jdbc/get-datasource {:dbtype "sqlite" :dbname "finance.db"}))
 
-(def ds (jdbc/get-datasource db))
+(jdbc/execute! ds ["CREATE TABLE IF NOT EXISTS finance (id INTEGER PRIMARY KEY AUTOINCREMENT, state TEXT)"])
 
-(defn create-table []
-  (jdbc/execute! ds ["
-                     create table finance (
-                                           id int auto_increment primary key,
-                                           bucket
-                                           )
-                     "]))
+(def headers {"Content-Type" "application/json"
+              "Access-Control-Allow-Origin" "*"
+              "Access-Control-Allow-Methods" "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+              "Access-Control-Allow-Headers" "Authorization, Content-Type"})
 
-(defroutes app-routes
-  (GET "/" [] "HEllo from backend"))
+(defn get-current-state [_]
+  (let  [state (jdbc/execute! ds ["SELECT state FROM finance ORDER BY id DESC LIMIT 1"])]
+    {:status 200
+     :headers headers
+     :body (json/write-str state)}))
 
-(defn start-server []
-  ;(create-table)
-  (run-jetty (wrap-defaults app-routes site-defaults) {:port 3000 :join? false}))
+(defn save-db-handler [request]
+  (let  [[_ body]  (:body request)
+         _         (jdbc/execute! ds ["insert into finance(state) values(?)" body])
+         fin       (jdbc/execute! ds ["SELECT * FROM finance"])]
+    {:status 200
+     :headers headers
+     :body {:saved "ok"
+            :id (count fin)}}))
 
-(defn -main []
-  (println "Server started")
-  (start-server))
+(defn options [_]
+  {:status 200
+   :headers headers})
+
+(def app
+  (->
+   (ring/ring-handler
+    (ring/router [["/current-state" {:get get-current-state}]
+                  ["/save-db" {:post save-db-handler
+                               :options options}]]))
+   (middleware/wrap-json-response)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (jetty/run-jetty (middleware/wrap-json-body app {:keywords? false})
+                   {:port 3000 :join? false})
+  (println "server running at 3000"))
